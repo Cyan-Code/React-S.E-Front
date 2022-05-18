8388945904a13dc5080c9ae705bb6fcfb626cf7c
Cyan-Code <lcarrionalvarez@gmail.com>
2022-05-18 13:55:26 -0500

    form sesion finished

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled, { css } from 'styled-components/macro'
+import { useForm } from '../hooks/useForm';
 import Button from './Button';
 
 const Intro = styled.div`
@@ -11,16 +12,55 @@ const btnCSS = css`
     margin-top: 2em;
 `;
 
-
 const Login = ({props}) => {
 
-    const startQuiz = () => props(true)
- 
+    const startQuiz = () => props(true);
+
+    const [{email, nombre}, handleInputChange] = useForm({
+        email:'',
+        nombre: '',
+    });
+
+    const handleSesion = () => {
+        console.log(email, nombre)
+        startQuiz();
+    }
+
     return (
         <Intro>
-            <h1>Take the quiz.</h1>
-            <h4>Whenever, you want.</h4>
-            <Button onClick={startQuiz} css={btnCSS}>Begin</Button>
+            <h1>Y... ¿Cual es el siguiente VideoJuego?</h1>
+            <form onSubmit={handleSesion}>
+                <span>Su correo: </span>
+                <div>
+                    <input
+                        type="text"
+                        className=""
+                        name="email"
+                        value={email}
+                        onChange={handleInputChange}
+                        placeholder="Ejemplo@.com"
+                    />
+                </div>
+                <span>Su Nombre: </span>
+                <div>
+                    <input
+                        type="text"
+                        className=""
+                        name="nombre"
+                        value={nombre}
+                        onChange={handleInputChange}
+                        placeholder="Su nombre"
+                    />
+                </div>
+            <Button type='submit' css={btnCSS}>Iniciar</Button>
+            </form>
+            <h4>
+                <ion-icon name="rocket-outline">   </ion-icon>
+                <ion-icon name="golf-outline">     </ion-icon>
+                <ion-icon name="sparkles-outline"> </ion-icon>
+                <ion-icon name="football-outline"> </ion-icon>
+                <ion-icon name="rocket-outline">   </ion-icon>
+            </h4>
         </Intro>
     )
 }
